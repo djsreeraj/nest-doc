9e9845b0e144ec110516fcd2bc7b6caeed8fecf8
djsreeraj <sreeraj011m@gmail.com>
2022-02-14 11:47:00 +0530

    added post req

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('customers')
 export class CustomersController {
     @Get()
     findAll(): string {
         return 'This action returns all customers';
+    }
+
+    @Post()
+    create(): string {
+        return 'This creates a new customer'
     }
 }
